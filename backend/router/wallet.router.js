b7cc574847de0aacf734fee5e106b9020754c12d
Kiran5258 <skirankumar97849@gmail.com>
2025-11-19 19:56:00 +0530

    wallet update

--- backend/router/wallet.router.js
+++ backend/router/wallet.router.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const walletCtrl = require("../controllers/wallet.controller");
 const { protect } = require("../middleware/auth.middleware");
+const { addMoneyWallet, verifyAddMoney, withdrawMoney, getWalletBalance, getRecentTransactions } = require("../controllers/wallet.controller");
 
 // Add money
-router.post("/add-money", protect, walletCtrl.addMoneyWallet);
+router.post("/add-money", protect, addMoneyWallet);
 
 // Verify Razorpay
-router.post("/verify", protect, walletCtrl.verifyAddMoney);
+router.post("/verify", protect, verifyAddMoney);
 
 // Withdraw
-router.post("/withdraw", protect, walletCtrl.withdrawMoney);
+router.post("/withdraw", protect,withdrawMoney);
 
 // Wallet balance
-router.get("/balance", protect, walletCtrl.getWalletBalance);
+router.get("/balance", protect, getWalletBalance);
 
 // Recent transactions
-router.get("/transactions", protect, walletCtrl.getRecentTransactions);
+router.get("/recenttransactions", protect, getRecentTransactions);
 
 module.exports = router;
