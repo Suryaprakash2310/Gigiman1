b58ccc7e85bed215813a6560658d22f3f0d3de4c
Kiran5258 <skirankumar97849@gmail.com>
2026-01-02 17:42:33 +0530

    socket

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,42 +1,57 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema=mongoose.Schema({
-    fullName:{
-        type:String,
-        required:false,
-    },
-    phoneNo:{
-        type:String,
-        required:true,
-        unique:false,
-    },
-    phoneMasked:{
-        type:String,
-        required:false,
-    },
-    location:{
-        type:{
-            type:String,
-            enum:["Point"],
-            default:"Point"
-        },
-        coordinates:{
-            type:[Number],
-            required:true,
-        },
-    },
-    address:{
-        type:String,
-    },
-    avator:{
-        type:String,
-        required:false,
-    },
-    isVerified:{
-        type:Boolean,
-        default:false,
-    }
-    
-},{timestamps:true})
+const UserSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required:false,
+  },
 
-module.exports=mongoose.model("User",UserSchema);
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+
+  phoneMasked: {
+    type: String,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point",
+    },
+    coordinates: {
+      type: [Number],
+      default: null,
+    },
+  },
+
+  address: {
+    type: String,
+    required:false,
+  },
+
+  avatar: {
+    type: String,
+    required:false,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  socketId: {
+    type: String,
+    default: null,
+  },
+
+  socketConnectedAt: {
+    type: Date,
+  },
+
+}, { timestamps: true });
+
+UserSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model("User", UserSchema);
